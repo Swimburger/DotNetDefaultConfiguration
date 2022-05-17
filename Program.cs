594e283a185f566db8345aef997e36c87f6f7da3
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-05-17 00:13:30 -0400

    Update code

--- Program.cs
+++ Program.cs
@@ -1,22 +1,45 @@
-using System.Text.Json;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var emailsSection = app.Configuration
-    .GetSection("Emails");
+EmailOptions GetEmailOptions(string emailSectionName)
+{
+    var emailOptions = new EmailOptions();
+    
+    var emailsSection = app.Configuration.GetSection("Emails");
+    emailsSection.GetSection("Defaults").Bind(emailOptions);
+    emailsSection.GetSection(emailSectionName).Bind(emailOptions);
+
+    return emailOptions;
+}
 
 
-var welcomeEmail = new EmailOptions();
-emailsSection.GetSection("Defaults").Bind(welcomeEmail);
-Console.WriteLine(JsonSerializer.Serialize(welcomeEmail));
+void LogEmailOptions(EmailOptions emailOptions)
+{
+    var logger = app.Logger;
+    logger.LogInformation(@"Logging Email Option:
+    FromEmail: {FromEmail}
+    FromName: {FromName}
+    ToEmail: {ToEmail}
+    ToName: {ToName}
+    Subject: {Subject}
+    Body: {Body}", 
+        emailOptions.FromEmail, 
+        emailOptions.FromName, 
+        emailOptions.ToEmail, 
+        emailOptions.ToName, 
+        emailOptions.Subject, 
+        emailOptions.Body
+    );
+}
 
-emailsSection.GetSection("Welcome").Bind(welcomeEmail);
-Console.WriteLine(JsonSerializer.Serialize(welcomeEmail));
+var welcomeEmail = GetEmailOptions("Welcome");
+LogEmailOptions(welcomeEmail);
 
-welcomeEmail = app.Configuration.GetSection("Emails")
-    .GetWithDefault<EmailOptions>("Defaults", "Welcome");
-Console.WriteLine(JsonSerializer.Serialize(welcomeEmail));
+var passwordResetEmail = GetEmailOptions("PasswordReset");
+LogEmailOptions(passwordResetEmail);
+
+var offerEmail = GetEmailOptions("Offer");
+LogEmailOptions(offerEmail);
 
 public class EmailOptions
 {
@@ -26,14 +49,4 @@ public class EmailOptions
     public string ToName { get; set; }
     public string Subject { get; set; }
     public string Body { get; set; }
-}
-
-public static class ConfigurationExtensions
-{
-    public static T GetWithDefault<T>(this IConfiguration configuration, string defaultSectionKey, string sectionKey)
-    {
-        var resultOptions = configuration.GetSection(defaultSectionKey).Get<T>();
-        configuration.GetSection(sectionKey).Bind(resultOptions);
-        return resultOptions;
-    }
 }
