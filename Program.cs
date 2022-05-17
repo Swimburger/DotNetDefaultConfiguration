dce69d3e0375088cffc56f725c708dd963ce4d4f
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-05-17 00:32:56 -0400

    Start

--- Program.cs
+++ Program.cs
@@ -1,28 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.Extensions.Configuration;
 
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+    
 EmailOptions GetEmailOptions(string emailSectionName)
 {
     var emailOptions = new EmailOptions();
     
-    var emailsSection = app.Configuration.GetSection("Emails");
-    emailsSection.GetSection("Defaults").Bind(emailOptions);
-    emailsSection.GetSection(emailSectionName).Bind(emailOptions);
+    var emailsSection = config.GetSection("Emails");
+    var emailSection = emailsSection.GetSection(emailSectionName);
+
+    emailOptions.FromEmail = emailSection["FromEmail"];
+    emailOptions.FromName = emailSection["FromName"];
+    emailOptions.ToEmail = emailSection["ToEmail"];
+    emailOptions.ToName = emailSection["ToName"];
+    emailOptions.Subject = emailSection["Subject"];
+    emailOptions.Body = emailSection["Body"];
 
     return emailOptions;
 }
 
-
 void LogEmailOptions(EmailOptions emailOptions)
 {
-    var logger = app.Logger;
-    logger.LogInformation(@"Logging Email Option:
-    FromEmail: {FromEmail}
-    FromName: {FromName}
-    ToEmail: {ToEmail}
-    ToName: {ToName}
-    Subject: {Subject}
-    Body: {Body}", 
+    Console.WriteLine(@"Logging Email Option:
+    FromEmail: {0}
+    FromName: {1}
+    ToEmail: {2}
+    ToName: {3}
+    Subject: {4}
+    Body: {5}", 
         emailOptions.FromEmail, 
         emailOptions.FromName, 
         emailOptions.ToEmail, 
